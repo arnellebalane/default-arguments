f9f4fc68d9be30d5adf1644be6ab6c57338336e7
Arnelle Balane <arnellebalane@gmail.com>
2016-02-10 08:28:46 +0800

    write package main script

---
+++ source/index.js
@@ -0,0 +1,12 @@
+function defaultArguments(fn, defaults = {}) {
+    let argnames = fn.toString().replace(/^.*\(|\)(.|\s)*$/g, '').split(/,\s*/g);
+    defaults = argnames.map(argname => defaults[argname]);
+    return function() {
+        let args = Array.from(arguments);
+        args = args.concat(defaults.slice(arguments.length));
+        return fn.apply(this, args);
+    };
+}
+
+
+module.exports = defaultArguments;
